339d3435d647ca1d2fdecf688379dddd5e0b4d1b
techbipin <techbipinrt2526@gmail.com>
2025-02-10 18:54:58 +0530

    First push uploaded

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+readme = ''
+try:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        readme = f.read()
+except FileNotFoundError:
+    print("Warning: README.rst not found. Skipping long description.")
 
 setup(
     name="data_encrypter",
@@ -9,17 +13,22 @@ setup(
     author="Mr. Bipin Rajesh Tatkare",
     author_email="techbipinrt2526@gmail.com",
     description="A simple data encryption-decryption package for Django models.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url="https://github.com/techbipin/data_encrypter.git",
     packages=find_packages(),
     install_requires=[
         "django==5.0",
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.11',
 )
